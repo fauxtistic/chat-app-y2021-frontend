a83574f46a4a182153a1be332e9e37730cd981b8
fauxtistic <56873702+fauxtistic@users.noreply.github.com>
2021-01-31 11:53:48 +0100

    initial commit

---
+++ src/app/chat/chat.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {ChatService} from './shared/chat.service';
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.scss']
+})
+export class ChatComponent implements OnInit {
+
+  message = new FormControl('');
+
+  constructor(private chatService: ChatService) { }
+
+  ngOnInit(): void {
+  }
+
+  sendMessage(): void {
+    console.log(this.message.value);
+    this.chatService.sendMessage(this.message.value);
+  }
+}
