20d49992d3362c13a5fe9b84cca9724c554f5d02
fauxtistic <56873702+fauxtistic@users.noreply.github.com>
2021-02-03 09:22:04 +0100

    new commit

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,19 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {FormControl} from '@angular/forms';
 import {ChatService} from './shared/chat.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss']
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit, OnDestroy {
 
   message = new FormControl('');
+  messages: string[] = [];
+  sub: Subscription = new Subscription();
 
   constructor(private chatService: ChatService) { }
 
   ngOnInit(): void {
+    this.sub = this.chatService.listenToMessages()
+      .subscribe(message => {
+        this.messages.push(message);
+      });
+  }
+
+  ngOnDestroy(): void {
+    if (this.sub) {
+      this.sub.unsubscribe();
+    }
   }
 
   sendMessage(): void {
