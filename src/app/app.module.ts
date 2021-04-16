93e98603dcd633604e2dc258803c0295ac21655a
fauxtistic <56873702+fauxtistic@users.noreply.github.com>
2021-04-16 11:21:49 +0200

    Getting ready for ngxs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import {Socket, SocketIoConfig, SocketIoModule} from 'ngx-socket-io';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {SharedModule} from './shared/shared.module';
+import {NgxsModule} from '@ngxs/store';
+import {environment} from '../environments/environment';
+import {NgxsLoggerPluginModule} from '@ngxs/logger-plugin';
 
 @Injectable()
 export class ChatSocket extends Socket {
@@ -35,6 +38,10 @@ export class StockSocket extends Socket {
     SocketIoModule,
     BrowserAnimationsModule,
     SharedModule,
+    NgxsModule.forRoot([], {
+      developmentMode: !environment.production
+    }),
+    NgxsLoggerPluginModule.forRoot()
   ],
   providers: [ChatSocket, StockSocket],
   bootstrap: [AppComponent]
